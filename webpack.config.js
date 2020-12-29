7ce7c0f25532463adc6d785cf7d1d41cdb71c0f1
清川　裕太郎 <yuu7353@gmail.com>
2020-12-29 10:39:00 +0900

    webpack設定

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,38 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
+  // 開発用の設定
   mode: 'development',
-  devtool: 'eval-source-map',
+
+  // エントリポイントとなるコード
   entry: './src/index.ts',
+
+  // バンドル後の js ファイルの出力先
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'index.js'
+  },
+
+  // ソースマップファイルの出力設定
+  devtool: 'source-map',
+
   module: {
     rules: [
+      // TypeScript ファイルの処理方法
       {
         test: /\.ts$/,
         use: 'ts-loader',
-        include: [path.resolve(__dirname, 'src')]
+        include: path.resolve(__dirname, 'src'),
+        exclude: /node_modules/
       }
     ]
   },
-  resolve: {
-    extensions: ['.ts', '.js']
-  },
-  output: {
-    publicPath: 'public',
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'public')
-  }
+
+  plugins: [
+    // HTML ファイルの出力設定
+    new HtmlWebpackPlugin({
+      template: './src/index.html'
+    })
+  ]
 };
