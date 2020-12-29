a12b13bebbea642dd390a576e021c5fa61640dac
清川　裕太郎 <yuu7353@gmail.com>
2020-12-29 17:23:54 +0900

    インターフェース

---
+++ src/type-inference.ts
@@ -0,0 +1,8 @@
+let inferredCityName = 'London';
+inferredCityName = 'Liverpool';
+
+let inferredCityPopulation = 8000000;
+inferredCityPopulation = 500000;
+
+let inferredisCapital = true;
+inferredisCapital = false;
