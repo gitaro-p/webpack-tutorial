73b3c76914051e0e73d71c46c94fa0848d527d44
清川　裕太郎 <yuu7353@gmail.com>
2020-12-30 23:49:13 +0900

    配列

---
+++ src/Array-types.ts
@@ -0,0 +1,12 @@
+var fruits1: string[] = ['apple', 'banana', 'mango'];
+
+var fruits2: Array<string>;
+fruits2 = ['apple', 'banana', 'mango'];
+
+console.log(fruits1);
+console.log(fruits2);
+
+var values: (string | number)[] = ['apple', 100, 'orange', 10];
+console.log(values);
+var values: Array<string | number> = ['apple', 100, 'orange', 10];
+console.log(values);
